76fed3f0478fad883967695e97fb01778ddb3dee
ren1023 <renyou.1023@gmail.com>
2023-11-20 23:16:59 +0800

    feat:修改上傳圖片的功能

--- api/upload.php
+++ api/upload.php
@@ -2,20 +2,33 @@
 
 echo $_POST['name'];
 echo "<br>";
-if(!empty($_FILES['img']['tmp_name'])){
+// 檢查是否有上傳的檔案
+if (!empty($_FILES['img']['tmp_name'])) {
+    // 輸出暫存檔案的路徑
     echo $_FILES['img']['tmp_name'];
     echo "<br>";
+    // 輸出上傳的檔案名稱
     echo $_FILES['img']['name'];
     echo "<br>";
+    // 輸出上傳的檔案類型
     echo $_FILES['img']['type'];
     echo "<br>";
+    // 輸出上傳的檔案大小
     echo $_FILES['img']['size'];
-    $tmp=explode(".",$_FILES['img']['name']);
-    $subname=".".end($tmp);
-    $filename=date("YmdHis").rand(10000,99999).$subname;
-    move_uploaded_file($_FILES['img']['tmp_name'],"../imgs/".$filename);
-    header("location:../upload.php?img=".$filename);
-}else{
+    // 從上傳的檔案名稱中取得檔案副檔名
+    $tmp = explode(".", $_FILES['img']['name']);
+    $subname = "." . end($tmp);
+
+    // 生成新的檔案名稱，包含日期、時間和隨機數字
+    $filename = date("YmdHis") . rand(10000, 99999) . $subname;
+
+    // 將上傳的檔案移動到目標資料夾
+    move_uploaded_file($_FILES['img']['tmp_name'], "../imgs/" . $filename);
+
+    // 重新導向到上傳頁面並將新的檔案名稱作為參數傳遞
+    header("location:../upload.php?img=" . $filename);
+} else {
+    // 如果沒有上傳檔案，重新導向到上傳頁面並傳遞錯誤訊息
     header ("location:../upload.php?err = 上傳失敗");
 }
 
