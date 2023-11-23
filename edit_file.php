a61f5ec5fafefbf4263819cf404ecc753bb25382
ren1023 <renyou.1023@gmail.com>
2023-11-24 00:11:40 +0800

    增加回列表的功能

--- edit_file.php
+++ edit_file.php
@@ -1,46 +1,101 @@
 <?php
 include_once "db.php";
+if (isset ($_GET ['id'])) { // 若檔案什麼都沒有時，什麼事都不執行
+    // 讀取檔案
+    $file = find('files', $_GET['id']);
+} else {
+    exit();
+}
 
 ?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title> 編輯檔案 </title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
+
 <body>
- <h1 class="header"> 編輯檔案 </h1>
- <!---- 建立你的表單及設定編碼 ----->
- <?php
+    <h1 class="header"> 編輯檔案 </h1>
 
-if(isset($_GET['err'])){
-    echo $_GET['err'];
-}
+    <div class="text-center">
+        <a href="manage.php"> 回列表 </a>
+    </div>
+    <!---- 建立你的表單及設定編碼 ----->
+    <?php
+    if (isset($_GET['err'])) {
+        echo $_GET['err'];
+    }
+    ?>
+    <!-- 當有取得 id 時，則要顯示資料在我的欄位中 -->
+    <form action="./api/edit_file.php" method="post" enctype="multipart/form-data">
+        <div class="col-6 mx-auto">
+            <table class="table">
+                <tr>
+                    <td> 媒體 </td>
+                    <td>
+                        <?php
+                        switch ($file['type']) {
+                            case "image/webp":
+                            case "image/jpeg":
+                            case "image/png":
+                            case "image/gif":
+                            case "image/bmp":
+                                $imgname = "./imgs/" . $file['name'];
+                                break;
+                            case 'msword':
+                                $imgname = "./icon/wordicon.png";
+                                break;
+                            case 'msexcel':
+                                $imgname = "./icon/msexcel.png";
+                                break;
+                            case 'msppt':
+                                $imgname = "./icon/msppt.png";
+                                break;
+                            case 'pdf':
+                                $imgname = "./icon/pdf.png";
+                                break;
+                            default:
+                                $imgname = "./icon/other.png";
+                        }
+                        ?>
 
-?>
-<form action="./api/edit_file.php" method="post" enctype="multipart/form-data">
+                        <img src="<?= $imgname; ?>" style="width:300px;height:200px;" alt="" srcset="">
+                        <input type="file" name="img" id="">
+                    </td>
+                </tr>
+                <tr>
+                    <td> 檔名 </td>
+                    <td><input type="text" name="name" value="<?= $file['name']; ?>"></td>
+                </tr>
+                <tr>
+                    <td> 說明 </td>
+                    <td><textarea name="desc" id="" style="width:300px;height:200px;"><?= $file['desc']; ?></textarea></td>
+                </tr>
 
-<input type="file" name="img" id="">
-<!-- 請輸入檔案名稱來更新 -->
-<input type="text" name="name" value="">
-<!-- 請輸入檔案描述來更新 -->
-<input type="text" name="desc" value="">
-<input type="submit" value="更新">
-</form>
+            </table>
+            <div class="text-center m-3"></div>
+            <input type="submit" value="更新">
+        </div>
 
-
-
-<!---- 建立一個連結來查看上傳後的圖檔 ---->  
-<?php
-
-if(isset($_GET['img'])){
-    echo "<img src='./imgs/{$_GET['img']}' style='width:250px;height:150px'>";
-}
-
-?>
-
+    </form>
+    <!---- 建立一個連結來查看上傳後的圖檔 ---->
+    <?php
+    if (isset($_GET['img'])) {
+        echo "<img src='./imgs/{$_GET['img']}' style='width:250px;height:150px'>";
+    }
+    ?>
 </body>
+
 </html>
--- manage.php
+++ manage.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * 1. 建立資料庫及資料表來儲存檔案資訊
  * 2. 建立上傳表單頁面
@@ -10,6 +11,7 @@ include_once "db.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,81 +19,85 @@ include_once "db.php";
     <title> 檔案管理功能 </title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap-grid.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
+
 <body>
-<h1 class="header"> 檔案管理練習 </h1>
-<!---- 建立上傳檔案表單及相關的檔案資訊存入資料表機制 ----->
-<h3><a href="upload.php"> 上傳檔案 </a></h3>
+    <h1 class="header"> 檔案管理練習 </h1>
+    <!---- 建立上傳檔案表單及相關的檔案資訊存入資料表機制 ----->
+    <h3><a href="upload.php"> 上傳檔案 </a></h3>
 
 
 
 
-<!---- 透過資料表來顯示檔案的資訊，並可對檔案執行更新或刪除的工作 ----->
-<?php
-$files=all('files');
-
-?>
-<div class="col-8 mx-auto">
-    <table class='table table-success'>
-        <tr>
-            <td>id</td>
-            <td> 檔名 </td>
-            <td> 類型 </td>
-            <td> 大小 </td>
-            <td> 描述 </td>
-            <td> 上傳時間 </td>
-            <td> 操作 </td>
-        </tr>
+    <!---- 透過資料表來顯示檔案的資訊，並可對檔案執行更新或刪除的工作 ----->
     <?php
-    foreach($files as $file){
-        switch($file['type']){
-            case "image/webp":
-            case "image/jpeg":
-            case "image/png":
-            case "image/gif":
-            case "image/bmp":
-                $imgname="./imgs/".$file['name'];
-            break;
-            case 'msword':
-                $imgname="./icon/wordicon.png";
-            break;
-            case 'msexcel':
-                $imgname="./icon/msexcel.png";
-            break;
-            case 'msppt':
-                $imgname="./icon/msppt.png";
-            break;
-            case 'pdf':
-                $imgname="./icon/pdf.png";
-            break;
-            default:
-                $imgname="./icon/other.png";
+    $files = all('files');
 
-        }
     ?>
-    <!--  -->
-        <tr>
-            <td><?=$file['id'];?></td>
-            <td><img class='thumbs' style="width:200px;heigh:200px" src="<?=$imgname;?>"></td>
-            <td><?=$file['type'];?></td>
-            <td><?=$file['size'];?></td>
-            <td><?=$file['desc'];?></td>
-            <td><?=$file['create_at'];?></td>
-            <td>
-                <button class="btn btn-info" onclick="location.href='./edit_file.php?id=<?=$file ['id'];?>'"> 編輯 </button>
-                <!-- <button class="btn btn-danger"><a href='./api/del_file.php?id=<?=$file ['id'];?>'> 刪除 </a></button> -->
-                <button class="btn btn-danger" onclick="location.href='./api/del_file.php?id=<?=$file ['id'];?>'"><a > 刪除 </a></button>
-            </td>
-        </tr>
-    <?php
-    }
-    ?>    
-    </table>
-</div>
+    <div class="col-8 mx-auto">
+        <table class='table table-success'>
+            <tr>
+                <td>id</td>
+                <td> 檔名 </td>
+                <td> 類型 </td>
+                <td> 大小 </td>
+                <td> 描述 </td>
+                <td> 上傳時間 </td>
+                <td> 操作 </td>
+            </tr>
+            <?php
+            foreach ($files as $file) {
+                switch ($file['type']) {
+                    case "image/webp":
+                    case "image/jpeg":
+                    case "image/png":
+                    case "image/gif":
+                    case "image/bmp":
+                        $imgname = "./imgs/" . $file['name'];
+                        break;
+                    case 'msword':
+                        $imgname = "./icon/wordicon.png";
+                        break;
+                    case 'msexcel':
+                        $imgname = "./icon/msexcel.png";
+                        break;
+                    case 'msppt':
+                        $imgname = "./icon/msppt.png";
+                        break;
+                    case 'pdf':
+                        $imgname = "./icon/pdf.png";
+                        break;
+                    default:
+                        $imgname = "./icon/other.png";
+                }
+            ?>
+                <!--  -->
+                <tr>
+                    <td><?= $file['id']; ?></td>
+                    <td><img class='thumbs' style="width:200px;heigh:200px" src="<?= $imgname; ?>"></td>
+                    <td><?= $file['type']; ?></td>
+                    <td><?= $file['size']; ?></td>
+                    <td><?= $file['desc']; ?></td>
+                    <td><?= $file['create_at']; ?></td>
+                    <td>
+                        <button class="btn btn-info" onclick="location.href='./edit_file.php?id=<?= $file ['id']; ?>'"> 編輯 </button>
+                        <!-- <button class="btn btn-danger"><a href='./api/del_file.php?id=<?= $file ['id']; ?>'> 刪除 </a></button> -->
+                        <button class="btn btn-danger" onclick="location.href='./api/del_file.php?id=<?= $file ['id']; ?>'"><a> 刪除 </a></button>
+                    </td>
+                </tr>
+            <?php
+            }
+            ?>
+        </table>
+    </div>
 
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
