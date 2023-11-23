0da822a472bbb76112d619609472fe9718a35eb5
ren1023 <renyou.1023@gmail.com>
2023-11-23 10:08:14 +0800

    引入bootstrap美化畫面

--- api/upload.php
+++ api/upload.php
@@ -1,23 +1,21 @@
 <?php
 
 include_once "../db.php";
-echo "files";
-
-echo $_POST['desc'];
+// echo "files";
+// echo $_POST['desc'];
 
 if(!empty($_FILES['img']['tmp_name'])){
-    echo $_FILES['img']['tmp_name'];
-    echo "<br>";
-    echo $_FILES['img']['name'];
-    echo "<br>";
-    echo $_FILES['img']['type'];
-    echo "<br>";
-    echo $_FILES['img']['size'];
+    // echo $_FILES['img']['tmp_name'];
+    // echo "<br>";
+    // echo $_FILES['img']['name'];
+    // echo "<br>";
+    // echo $_FILES['img']['type'];
+    // echo "<br>";
+    // echo $_FILES['img']['size'];
     $subname=".".[end (explode (".",$_FILES ['img']['name']))];// 取陣列的最後一個值
     $filename=date("YmdHis").rand(10000,99999).$subname;
     move_uploaded_file($_FILES['img']['tmp_name'],"../imgs/".$filename);
-
-    
+ 
     
     // header("location:../upload.php?img=.$filename");
     header("location:../manage.php");
