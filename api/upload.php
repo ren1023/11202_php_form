eff4fc41bd9ed6040bff2fe76ea42a6d648c38d1
ren1023 <renyou.1023@gmail.com>
2023-11-23 10:23:32 +0800

    引入bootstrap美化畫面

--- api/upload.php
+++ api/upload.php
@@ -1,28 +1,30 @@
 <?php
-
 include_once "../db.php";
-// echo "files";
-// echo $_POST['desc'];
-
+/* echo $_POST['name'];
+echo "<br>"; */
 if(!empty($_FILES['img']['tmp_name'])){
-    // echo $_FILES['img']['tmp_name'];
-    // echo "<br>";
-    // echo $_FILES['img']['name'];
-    // echo "<br>";
-    // echo $_FILES['img']['type'];
-    // echo "<br>";
-    // echo $_FILES['img']['size'];
-    $subname=".".[end (explode (".",$_FILES ['img']['name']))];// 取陣列的最後一個值
+/*     echo $_FILES['img']['tmp_name'];
+    echo "<br>";
+    echo $_FILES['img']['name'];
+    echo "<br>";
+    echo $_FILES['img']['type'];
+    echo "<br>";
+    echo $_FILES['img']['size']; */
+    $tmp=explode(".",$_FILES['img']['name']);
+    $subname=".".end($tmp);
     $filename=date("YmdHis").rand(10000,99999).$subname;
     move_uploaded_file($_FILES['img']['tmp_name'],"../imgs/".$filename);
- 
-    
-    // header("location:../upload.php?img=.$filename");
-    header("location:../manage.php");
-    
-}else{
 
-    header ("locatoin:../uploade.php?err = 上傳失敗");
-    
+    $file=['name'=>$filename,
+            'type'=>$_FILES['img']['type'],
+            'size'=>$_FILES['img']['size'],
+            'desc'=>$_POST['desc']];
+
+    insert('files',$file);
+    //header("location:../upload.php?img=".$filename);
+    header("location:../manage.php");
+}else{
+    header ("location:../upload.php?err = 上傳失敗");
 }
+
 ?>
