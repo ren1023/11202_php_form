7c8b42127b98f71d731185293018d7f41abbd6c7
ren1023 <renyou.1023@gmail.com>
2023-11-23 11:30:39 +0800

    增加非圖形的檔案判斷

--- api/upload.php
+++ api/upload.php
@@ -15,6 +15,45 @@ if(!empty($_FILES['img']['tmp_name'])){
     $filename=date("YmdHis").rand(10000,99999).$subname;
     move_uploaded_file($_FILES['img']['tmp_name'],"../imgs/".$filename);
 
+// 錯誤訊息
+
+/**
+     * application/vnd.openxmlformats-officedocument.wordprocessingml.document - word
+     * application/vnd.openxmlformats-officedocument.spreadsheetml.sheet-excel
+     * application/vnd.openxmlformats-officedocument.presentationml.presentation-ppt
+     * application/pdf - pdf
+     */
+// 錯誤訊息
+     switch($_FILES['img']['type']){
+        case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+            $type="msword";
+        break;
+        case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+            $type='msexcel';
+        break;
+        case "application/vnd.openxmlformats-officedocument.presentationml.presentation":
+            $type='msppt';
+        break;
+        case "application/pdf":
+            $type='pdf';
+        break;
+        case "image/webp":
+        case "image/jpeg":
+        case "image/png":
+        case "image/gif":
+        case "image/bmp":
+            $type=$_FILES['img']['type'];
+        break;
+        default:
+            $type='other';
+
+     }
+
+
+
+
+    
+
     $file=['name'=>$filename,
             'type'=>$_FILES['img']['type'],
             'size'=>$_FILES['img']['size'],
@@ -24,7 +63,7 @@ if(!empty($_FILES['img']['tmp_name'])){
     //header("location:../upload.php?img=".$filename);
     header("location:../manage.php");
 }else{
-    header("location:../upload.php?err=上傳失敗");
+    header ("location:../upload.php?err = 上傳失敗");
 }
 
 ?>
